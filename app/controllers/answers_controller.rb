800eb4b0c9534b3c1ae3c634fd076d3f6427c24e
Fe2plus <unspeakablefe@gmail.com>
2017-07-04 17:37:17 +0900

    before bootstrap

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,9 @@
 class AnswersController < ApplicationController
   def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get 'answers/index'
+  get 'answers/new'
+
+  resources :users
 
 end
