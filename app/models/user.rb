27a2eb3232ce5abca1ee122f40b9edc8be5b33cf
Fe2plus <unspeakablefe@gmail.com>
2017-08-03 15:42:24 +0900

    before 9.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
   has_one :profile
   has_many :posts
   has_many :replies
 
+#ランダムなトークンを返す。
   def User.new_token
     SecureRandom.urlsafe_base64
   end
+
+#永続セッションのためにユーザーをデータベースに記憶する？
+ def remember
+   self.remember_token = User.new_token
+   update_attribute(:remember_digest, User.digest(remember_token))
+ end
 end
