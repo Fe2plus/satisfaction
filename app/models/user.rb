f791df90c6f8ed38bc6237bba7c6df9f7c0438f6
Fe2plus <unspeakablefe@gmail.com>
2017-08-05 17:01:43 +0900

    before 9.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
   has_many :posts
   has_many :replies
 
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 #ランダムなトークンを返す。
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -15,4 +22,14 @@ class User < ApplicationRecord
    self.remember_token = User.new_token
    update_attribute(:remember_digest, User.digest(remember_token))
  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
