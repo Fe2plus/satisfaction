9493e237f419ae2e88dc683b06134bff975c9688
Fe2plus <unspeakablefe@gmail.com>
2017-09-03 23:59:06 +0900

    solve papertrail_problem

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   # photoをattachファイルとする。stylesで画像サイズを定義できる
   has_attached_file :user_image, styles: { medium: "300x300>", thumb: "100x100>" }
