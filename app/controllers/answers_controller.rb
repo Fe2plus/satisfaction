e30364eb0f95db90d6a8f8dabd5139f42beb8f5f
Fe2plus <unspeakablefe@gmail.com>
2017-09-18 22:17:35 +0900

    make account image

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,4 +2,9 @@ class AnswersController < ApplicationController
   def index
     @users = User.all
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, profile_attributes: [:user_image])
+  end
 end
